1625c2f1120231b92ee8c2a49c037469df486272
gonza <gonza56d@gmail.com>
2022-06-26 15:02:58 -0300

    schemas

--- zira/api/schemas.py
+++ zira/api/schemas.py
@@ -1,46 +1,8 @@
-from datetime import date, datetime
-from typing import List
-from uuid import UUID
+from pydantic.dataclasses import dataclass as pydantic_dataclass
 
-from pydantic import BaseModel
-
-from zira.core.models import Period
+from zira.core.models import Daily, Period, Spending
 
 
-class SpendingSchema(BaseModel):
-
-    id: UUID
-    time: datetime
-    amount: float
-    daily_id: UUID
-
-
-class DailySchema(BaseModel):
-
-    id: UUID
-    date: date
-    period_id: UUID
-    spendings: List[SpendingSchema]
-    spendings_amount: float
-
-
-class PeriodSchema(BaseModel):
-
-    id: UUID
-    date_from: date
-    date_to: date
-    budget: float
-    dailies: List[DailySchema]
-    current_balance: float
-    dailies_spendings_amount: float
-    max_daily_spend: float
-    total_days: int
-    days_left: int
-    current_days: int
-    active: bool
-
-    @staticmethod
-    def dump(period: Period):
-        return PeriodSchema(
-
-        )
+DailySchema = pydantic_dataclass(Daily)
+PeriodSchema = pydantic_dataclass(Period)
+SpendingSchema = pydantic_dataclass(Spending)
